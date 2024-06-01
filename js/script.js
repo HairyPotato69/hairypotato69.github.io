662531fdc8d06c004877fd7554c5116b2a5a4148
HairyPotato69 <jimmyding9719@gmail.com>
2024-06-01 21:27:41 +0800

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    // Selects an element on the webpage
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open")
+}
